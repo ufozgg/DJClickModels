c4b9780f6fa25bf9b958e7638bb6f3130641c299
ufozgg <ufozgg@qq.com>
2019-05-17 18:33:46 +0800

    merge

--- run3.sh
+++ run3.sh
@@ -1,12 +1,12 @@
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/100_999M_train.ucf,../tmp_data/100_999M_test.ucf,../tmp_data/100_999M_val.ucf -r 100 >../output/vcm+vcmlayout100_999M
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/10_99M_train.ucf,../tmp_data/10_99M_test.ucf,../tmp_data/10_99M_val.ucf -r 100 >../output/vcm+vcmlayout10_99M
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/1000_9999M_train.ucf,../tmp_data/1000_9999M_test.ucf,../tmp_data/1000_9999M_val.ucf -r 100 >../output/vcm+vcmlayout1000_9999M
+time ./main -m vscm -f false -l ucf -i ../tmp_data/100_999M_train.ucf,../tmp_data/100_999M_test.ucf,../tmp_data/100_999M_val.ucf -r 100 >../output/vscm100_999M
+time ./main -m vscm -f false -l ucf -i ../tmp_data/10_99M_train.ucf,../tmp_data/10_99M_test.ucf,../tmp_data/10_99M_val.ucf -r 100 >../output/vscm10_99M
+time ./main -m vscm -f false -l ucf -i ../tmp_data/1000_9999M_train.ucf,../tmp_data/1000_9999M_test.ucf,../tmp_data/1000_9999M_val.ucf -r 100 >../output/vscm1000_9999M
 
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/100_999HK_train.ucf,../tmp_data/100_999HK_test.ucf,../tmp_data/100_999HK_val.ucf -r 100 >../output/vcm+vcmlayout100_999HK
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/10_99HK_train.ucf,../tmp_data/10_99HK_test.ucf,../tmp_data/10_99HK_val.ucf -r 100 >../output/vcm+vcmlayout10_99HK
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/1000_9999HK_train.ucf,../tmp_data/1000_9999HK_test.ucf,../tmp_data/1000_9999HK_val.ucf -r 100 >../output/vcm+vcmlayout1000_9999HK
+time ./main -m vscm -f false -l ucf -i ../tmp_data/100_999HK_train.ucf,../tmp_data/100_999HK_test.ucf,../tmp_data/100_999HK_val.ucf -r 100 >../output/vscm100_999HK
+time ./main -m vscm -f false -l ucf -i ../tmp_data/10_99HK_train.ucf,../tmp_data/10_99HK_test.ucf,../tmp_data/10_99HK_val.ucf -r 100 >../output/vscm10_99HK
+time ./main -m vscm -f false -l ucf -i ../tmp_data/1000_9999HK_train.ucf,../tmp_data/1000_9999HK_test.ucf,../tmp_data/1000_9999HK_val.ucf -r 100 >../output/vscm1000_9999HK
 
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/100_999M+_train.ucf,../tmp_data/100_999M+_test.ucf,../tmp_data/100_999M+_val.ucf -r 100 >../output/vcm+vcmlayout100_999M+
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/10_99M+_train.ucf,../tmp_data/10_99M+_test.ucf,../tmp_data/10_99M+_val.ucf -r 100 >../output/vcm+vcmlayout10_99M+
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/1000_9999M+_train.ucf,../tmp_data/1000_9999M+_test.ucf,../tmp_data/1000_9999M+_val.ucf -r 100 >../output/vcm+vcmlayout1000_9999M+
-time ./main -m vcm,vcmlayout -f false -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 100 >../output/vcm+vcmlayout10_9999M+
+time ./main -m vscm -f false -l ucf -i ../tmp_data/100_999M+_train.ucf,../tmp_data/100_999M+_test.ucf,../tmp_data/100_999M+_val.ucf -r 100 >../output/vscm100_999M+
+time ./main -m vscm -f false -l ucf -i ../tmp_data/10_99M+_train.ucf,../tmp_data/10_99M+_test.ucf,../tmp_data/10_99M+_val.ucf -r 100 >../output/vscm10_99M+
+time ./main -m vscm -f false -l ucf -i ../tmp_data/1000_9999M+_train.ucf,../tmp_data/1000_9999M+_test.ucf,../tmp_data/1000_9999M+_val.ucf -r 100 >../output/vscm1000_9999M+
+time ./main -m vscm -f false -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 100 >../output/vscm10_9999M+
