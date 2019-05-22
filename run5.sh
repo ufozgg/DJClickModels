e7d66d0a154592eb17be4c835a9e5d5ec3760034
ufozgg <ufozgg@qq.com>
2019-05-22 13:01:09 +0800

    run6

---
+++ run5.sh
@@ -0,0 +1,34 @@
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 011111111 -p 2 1>../output/TM/011111_111_20 2>../output/TM/err20_1 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 011111111 -p 1 1>../output/TM/011111_111_10 2>../output/TM/err10_1 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 011111111 -p 0.5 1>../output/TM/011111_111_05 2>../output/TM/err05_1 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 001111111 -p 2 1>../output/TM/001111_111_20 2>../output/TM/err20_2 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 001111111 -p 1 1>../output/TM/001111_111_10 2>../output/TM/err10_2 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 001111111 -p 0.5 1>../output/TM/001111_111_05 2>../output/TM/err05_2
+sleep 15m
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000111111 -p 2 1>../output/TM/000111_111_20 2>../output/TM/err20_3 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000111111 -p 1 1>../output/TM/000111_111_10 2>../output/TM/err10_3 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000111111 -p 0.5 1>../output/TM/000111_111_05 2>../output/TM/err05_3 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000011111 -p 2 1>../output/TM/000011_111_20 2>../output/TM/err20_4 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000011111 -p 1 1>../output/TM/000011_111_10 2>../output/TM/err10_4 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000011111 -p 0.5 1>../output/TM/000011_111_05 2>../output/TM/err05_4  
+sleep 15m
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 110011111 -p 2 1>../output/TM/110011_111_20 2>../output/TM/err20_5 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 110011111 -p 1 1>../output/TM/110011_111_10 2>../output/TM/err10_5 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 110011111 -p 0.5 1>../output/TM/110011_111_05 2>../output/TM/err05_5 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 011111011 -p 2 1>../output/TM/011111_011_20 2>../output/TM/err20_1_1 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 011111011 -p 1 1>../output/TM/011111_011_10 2>../output/TM/err10_1_1 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 011111011 -p 0.5 1>../output/TM/011111_011_05 2>../output/TM/err05_1_1
+sleep 15m
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 001111011 -p 2 1>../output/TM/001111_011_20 2>../output/TM/err20_1_2 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 001111011 -p 1 1>../output/TM/001111_011_10 2>../output/TM/err10_1_2 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 001111011 -p 0.5 1>../output/TM/001111_011_05 2>../output/TM/err05_1_2  &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000111011 -p 2 1>../output/TM/000111_011_20 2>../output/TM/err20_1_3 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000111011 -p 1 1>../output/TM/000111_011_10 2>../output/TM/err10_1_3 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000111011 -p 0.5 1>../output/TM/000111_011_05 2>../output/TM/err05_1_3 
+sleep 15m
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000011011 -p 2 1>../output/TM/000011_011_20 2>../output/TM/err20_1_4 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000011011 -p 1 1>../output/TM/000011_011_10 2>../output/TM/err10_1_4 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 000011011 -p 0.5 1>../output/TM/000011_011_05 2>../output/TM/err05_1_4 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 110011011 -p 2 1>../output/TM/110011_011_20 2>../output/TM/err20_1_5 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 110011011 -p 1 1>../output/TM/110011_011_10 2>../output/TM/err10_1_5 &
+time ./main -l ucf -i ../tmp_data/10_9999TM_train.ucf,../tmp_data/10_9999TM_test.ucf,../tmp_data/10_9999TM_val.ucf -r 60 -m mvcm -e 110011011 -p 0.5 1>../output/TM/110011_011_05 2>../output/TM/err05_1_5 
